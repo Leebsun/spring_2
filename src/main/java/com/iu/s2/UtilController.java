e928118b616dc80ae111a5dfa5ec43bd83776439
Leebsun <gkgktmdfl@naver.com>
2017-12-06 13:50:06 +0900

    final

--- src/main/java/com/iu/s2/UtilController.java
+++ src/main/java/com/iu/s2/UtilController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.iu.file.FileService;
 import com.iu.file.PhotoDTO;
@@ -26,11 +27,22 @@ public class UtilController {
 	@Inject
 	private FileService fileService;
 	
-	@RequestMapping(value="fileDelete",method=RequestMethod.POST)
-	public String fileDelete(Model model, int fnum,HttpSession session){
-		String message="Fail";
+	@RequestMapping(value="download")
+	public ModelAndView fileDowload(String filename,String oriname, HttpSession session){
+		String filePath = session.getServletContext().getRealPath("resources/upload");
+		File file = new File(filePath, filename);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("down", file);
+		mv.addObject("oriname", oriname);
+		mv.setViewName("filedown");
+		return mv;
+	}
+	
+	@RequestMapping(value="fileDelete", method=RequestMethod.POST)
+	public String fileDelete(Model model,int fnum, HttpSession session){
+		String message="Fail";		
 		try {
-			int result=fileService.fileDelete(fnum, session);
+			int result = fileService.fileDelete(fnum, session);
 			if(result>0){
 				message="Success";
 			}
@@ -45,13 +57,12 @@ public class UtilController {
 	}
 	
 	
-	
 	@RequestMapping(value="photoUpload", method=RequestMethod.POST)
 	public String smartEditor(PhotoDTO photoDTO, HttpSession session){
-	
+		
 		//service
-		String filePath = session.getServletContext().getRealPath("/");
-		filePath=filePath+"resources"+File.separator+"upload";
+		String filePath = session.getServletContext().getRealPath("resources/upload");
+		//filePath=filePath+"resources/upload";
 		File file = new File(filePath);
 		System.out.println(filePath);
 		if(!file.exists()){
@@ -63,7 +74,7 @@ public class UtilController {
 		StringBuffer stringBuffer = new StringBuffer();
 		try {
 			fileName=fs.save1(filePath, photoDTO.getFiledata());
-			//result="&bNewLine=true&sFileName="+photoDTO.getFiledata().getOriginalFilename()+"&sFileURL=/"+session.getServletContext().getContextPath()+File.separator+"upload"+File.separator+fileName;
+			//result="&bNewLine=true&sFileName="+photoDTO.getFiledata().getOriginalFilename()+"&sFileURL="+session.getServletContext().getContextPath()+File.separator+"upload"+File.separator+fileName;
 			stringBuffer.append("&bNewLine=true&sFileName=");
 			stringBuffer.append(photoDTO.getFiledata().getOriginalFilename());
 			stringBuffer.append("&sFileURL=");
@@ -76,7 +87,6 @@ public class UtilController {
 			// TODO Auto-generated catch block
 			
 		}
-		
 		
 		System.out.println(photoDTO.getCallback()+photoDTO.getCallback_func()+stringBuffer.toString());
 		return "redirect:"+photoDTO.getCallback()+photoDTO.getCallback_func()+stringBuffer.toString();
