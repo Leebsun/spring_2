e5715080179547ac1dfd09046fb90c3a36905971
Leebsun <gkgktmdfl@naver.com>
2017-12-08 15:09:00 +0900

    final

--- src/main/java/com/iu/member/MemberService.java
+++ src/main/java/com/iu/member/MemberService.java
@@ -4,13 +4,29 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
+import com.iu.file.FileDAO;
+import com.iu.util.FileSaver;
+
 @Service
 public class MemberService {
 	
 	
-	 @Inject
+	@Inject
 	private MemberDAO memberDAO;
+	@Inject
+	private FileDAO fileDAO;
+	@Inject
+	private FileSaver fileSaver;
 	
 	
+	public void login(){
+		
+	}
+	
+	public void join(){
+		
+	}
+	
+	public
 
 }
